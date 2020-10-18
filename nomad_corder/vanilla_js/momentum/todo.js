6ed7243838fce2638a7b16c8c26ebd626e09baf0
SeoChanhee <uiuxchanh@gmail.com>
2020-10-18 23:10:13 +0900

    first commit

---
+++ nomad_corder/vanilla_js/momentum/todo.js
@@ -0,0 +1,18 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = form.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList")
+
+const TODOS_LS = 'toDos';
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if(toDos !== null){
+
+  }
+}
+
+function init() {
+  loadToDos();
+}
+
+init();
