8cc0c8848f2f222ef29cdacc6b93f3136b1e5c5f
SeoChanhee <uiuxchanh@gmail.com>
2020-10-20 15:34:09 +0900

    Add clock2.js

---
+++ nomad_corder/vanilla_js/momentum/clock2.js
@@ -0,0 +1,13 @@
+const clockContainer = document.querySelector(".js-clock"),
+      clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+}
+      
+
+function init() {
+  
+}
+
+init();
